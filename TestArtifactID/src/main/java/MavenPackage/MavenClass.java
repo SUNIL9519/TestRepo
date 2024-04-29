85f8984420e1db99921a20fbd8934933f741d39e
Sunil Kumar Chhualsingh <rajucsingh@gmail.com>
2024-04-30 01:10:20 +0530

    Added 3 times

--- TestArtifactID/src/main/java/MavenPackage/MavenClass.java
+++ TestArtifactID/src/main/java/MavenPackage/MavenClass.java
@@ -4,6 +4,8 @@ public class MavenClass {
 
 	public static void main(String[] args) {
 		System.out.println("Hello Maven");
+		System.out.println("Hello Maven");
+		System.out.println("Hello Maven");
 		
 	}
 
